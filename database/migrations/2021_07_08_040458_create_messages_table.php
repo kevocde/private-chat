b191f7bfb1391f25b77b4dbd0e3804af14b02b34
kevocde <kevocde@gmail.com>
2021-07-07 23:09:51 -0500

    Creadas migraciones según modelo

---
+++ database/migrations/2021_07_08_040458_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigInteger('id', true);
+            $table->bigInteger('participant_id')->index('messages_participant_id');
+            $table->char('type', 3)->index('messages_type')->comment('TXT: Texto
+IMG: Imagen
+AUD: Audio
+VID: Video');
+            $table->text('content');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
---
+++ database/migrations/2021_07_08_040458_create_participants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->bigInteger('id', true);
+            $table->bigInteger('guest_id')->index('participants_guest_id');
+            $table->bigInteger('room_id')->index('participants_room_id');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
