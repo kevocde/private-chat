4e73a3416aa15bd129b31b9eecffe77f15cae657
kevocde <kevocde@gmail.com>
2021-07-23 08:22:04 -0500

    Realizados cambios en barra de navegación y estilos

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
         alias: {
             '~': path.resolve('resources/js')
         }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(postcss)$/,
+                use: [
+                    'vue-style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader'
+                ]
+            }
+        ],
     }
 }
