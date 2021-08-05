ba47c7cec7340c1e8203240909d45069f4051eb3
kevocde <kevocde@gmail.com>
2021-08-05 08:41:00 -0500

    Añadidos estilos para chat

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -2,8 +2,8 @@
     <div class="header-component-container">
         <nav class="header__navigator">
             <ul class="header__navigator__list">
-                <li class="header__navigator__list__item active"><a href="" class="text-gray-600">Dashboard</a></li>
-                <li class="header__navigator__list__item"><a href="" class="text-gray-600">Configurations</a></li>
+                <li class="header__navigator__list__item active"><a href="">Dashboard</a></li>
+                <li class="header__navigator__list__item"><a href="">Configurations</a></li>
             </ul>
         </nav>
     </div>
@@ -20,19 +20,19 @@ export default {
         @apply bg-white h-16;
     }
 
-        .header-component-container .header__navigator {
-            @apply col-span-4 h-full px-4;
-        }
+    .header__navigator {
+        @apply col-span-4 h-full px-4;
+    }
 
-            .header-component-container .header__navigator .header__navigator__list {
-                @apply flex h-full;
-            }
+    .header__navigator__list {
+        @apply flex h-full;
+    }
 
-                .header-component-container .header__navigator .header__navigator__list .header__navigator__list__item {
-                    @apply flex items-center px-4 transition transition duration-500 ease-in-out hover:bg-indigo-200;
-                }
+    .header__navigator__list__item {
+        @apply flex items-center px-4 transition transition duration-200 ease-in-out font-semibold text-gray-500 border-b-2 border-transparent hover:border-indigo-500;
+    }
 
-                .header-component-container .header__navigator .header__navigator__list .header__navigator__list__item.active {
-                    @apply border-b-2 border-indigo-500;
-                }
+    .header__navigator__list__item.active {
+        @apply border-b-2 border-indigo-500;
+    }
 </style>
--- resources/js/Components/LateralNav.vue
+++ resources/js/Components/LateralNav.vue
@@ -7,10 +7,25 @@
         <div class="nav-container">
             <form action="#" class="search-form">
                 <div class="search-form__control">
+                    <input type="text" class="search-form__control__input" placeholder="Search by name">
                     <div class="search-form__control__icon"><i data-feather="search"></i></div>
-                    <input type="text" class="search-form__control__input">
                 </div>
             </form>
+        </div>
+        <div class="chat-container">
+            <ul class="chat-list">
+                <li class="chat-list__item">
+                    <div class="chat-list__item__profile" style="background-image: url('https://fakeimg.pl/250x250/')"></div>
+                    <div class="chat-list__item__resume">
+                        <h3 class="chat-list__item__resume__title">Jobs West</h3>
+                        <p class="chat-list__item__resume__message">Ross C. Aenean massa</p>
+                    </div>
+                    <div class="chat-list__item__details">
+                        <span class="chat-list__item__details__hour">17:04</span>
+                        <span class="chat-list__item__details__message-count">2</span>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
 </template>
@@ -23,7 +38,7 @@ export default {
 
 <style scoped lang="postcss">
     .logo-container {
-        @apply w-full inline-flex justify-center items-center bg-indigo-400 text-center max-h-20 p-2;
+        @apply w-full inline-flex justify-center items-center bg-indigo-500 text-center max-h-20 p-2;
     }
 
     .logo-container__icon {
@@ -38,11 +53,59 @@ export default {
         @apply w-full py-4;
     }
 
-    .nav-container .search-form {
+    .search-form {
         @apply w-full flex justify-center;
     }
 
     .search-form__control {
-        @apply px-2 inline-flex justify-center items-center;
+        @apply w-full inline-flex justify-between items-center p-5 border-b-2;
+    }
+
+    .search-form__control__input {
+        @apply flex-1 mr-4 border-0 focus:ring-0 font-semibold text-gray-500 placeholder-gray-400;
+    }
+
+    .search-form__control__icon {
+        @apply text-center text-gray-400;
+    }
+
+    .chat-container {
+        @apply w-full;
+    }
+
+    .chat-list {
+        @apply w-full px-3;
+    }
+
+    .chat-list__item {
+        @apply flex justify-around items-center cursor-pointer bg-white p-3 rounded-md hover:bg-gray-100;
+    }
+
+    .chat-list__item__profile {
+        @apply w-10 h-10 rounded-full;
+    }
+
+    .chat-list__item__resume {
+    }
+
+    .chat-list__item__resume__title {
+        @apply font-bold text-sm text-gray-700 w-full;
+    }
+
+    .chat-list__item__resume__message {
+        @apply text-xs text-gray-500 w-full;
+    }
+
+    .chat-list__item__details {
+        @apply flex-row justify-center items-center;
+    }
+
+    .chat-list__item__details__hour {
+        @apply block text-xs text-gray-400;
+    }
+
+    .chat-list__item__details__message-count {
+        @apply block bg-red-500 text-red-50 p-1 w-5 h-5 rounded-full text-center font-semibold;
+        font-size: 0.6rem;
     }
 </style>
